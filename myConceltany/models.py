30c14d50fed04506a96092ac9de2225fe7cda6a5
Biruk Chali <birukchali86@gmail.com>
2024-02-25 06:44:12 -0600

    getting data from the data base

---
+++ myConceltany/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=128)  # Adjusted max_length for password
